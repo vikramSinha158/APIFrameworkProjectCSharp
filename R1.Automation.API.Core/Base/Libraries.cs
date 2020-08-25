367b6753892786162cba15412bad0dce12efe16c
Vikram Kumar Sinha <VSinha2@R1RCM.COM>
2020-08-25 12:55:10 +0530

    api

---
+++ R1.Automation.API.Core/Base/Libraries.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Threading.Tasks;
+using RestSharp.Serialization.Json;
+using System.Text;
+
+namespace R1.Automation.API.Core.Base
+{
+   public class Libraries
+    {
+        /// <summary>This method is used for initialized request with method</summary>
+        /// <param name="url"></param>
+        /// <param name="method"></param>
+        /// <returns>This returns rest request with url and method</returns>
+        public IRestRequest GetRequest(string url, Method method)
+        {
+            switch (method)
+            {
+                case Method.GET:
+                    return new RestRequest(url, Method.GET);
+                case Method.POST:
+                    return new RestRequest(url, Method.POST);
+                case Method.PUT:
+                    return new RestRequest(url, Method.PUT);
+                case Method.PATCH:
+                    return new RestRequest(url, Method.PATCH);
+                case Method.DELETE:
+                    return new RestRequest(url, Method.DELETE);
+                default:
+                    return null;
+            }
+
+        }
+
+        /// <summary>This method is used for Add Path Parameter with request</summary>
+        /// <param name="request"></param>
+        /// <param name="pathParams"></param>
+        public void AddPathParameter(IRestRequest request, Dictionary<String, String> pathParams)
+        {
+            foreach (var param in pathParams)
+                request.AddParameter(param.Key, param.Value, ParameterType.UrlSegment);
+        }
+
+        /// <summary>This method is used for add body with request</summary>
+        /// <param name="request"></param>
+        /// <param name="addBody"></param>
+        public void AddPostRequestBody(IRestRequest request, string addBody)
+        {
+            request.RequestFormat = DataFormat.Json;
+            request.AddParameter("application/json", addBody, ParameterType.RequestBody);
+
+        }
+
+        /// <summary>This method is used for add headers with request</summary>
+        /// <param name="request"></param>
+        /// <param name="headers"></param>
+        public void AddHeadersForGetPost(IRestRequest request, Dictionary<String, String> headers)
+        {
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeaders(headers);
+        }
+
+
+
+        /// <summary>This method is used for execute request</summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <returns>This return response</returns>
+        public async Task<IRestResponse<T>> ExecuteAsyncRequest<T>(RestClient client, IRestRequest request) where T : class, new()
+        {
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse<T>>();
+
+            client.ExecuteAsync<T>(request, restResponse =>
+            {
+                if (restResponse.ErrorException != null)
+                {
+                    const string message = "Error retrieving response.";
+                    throw new ApplicationException(message, restResponse.ErrorException);
+                }
+
+                taskCompletionSource.SetResult(restResponse);
+            });
+
+            return await taskCompletionSource.Task;
+        }
+
+        /// <summary>This method is used for Deserialize Response</summary>
+        /// <param name="restResponse"></param>
+        /// <returns>This return Deserialized response</returns>
+        public Dictionary<string, string> DeserializeResponse(IRestResponse restResponse)
+        {
+            var JSONObj = new JsonDeserializer().Deserialize<Dictionary<string, string>>(restResponse);
+
+            return JSONObj;
+        }
+
+        /// <summary>This method is used for Get Response Object</summary>
+        /// <param name="response"></param>
+        /// <param name="responseObject"></param>
+        /// <returns>This returns Response Object in string</returns>
+        public string GetResponseObject(IRestResponse response, string responseObject)
+        {
+            JObject obs = JObject.Parse(response.Content);
+            return obs[responseObject].ToString();
+        }
+
+        /// <summary>This method is used for add query parameter in request</summary>
+        /// <param name="request"></param>
+        /// <param name="pathParams"></param>
+        public void AddQueryParameter(IRestRequest request, Dictionary<String, String> pathParams)
+        {
+            foreach (var param in pathParams)
+                request.AddQueryParameter(param.Key, param.Value);
+        }
+    }
+}
---
+++ R1.Automation.API.Core/Utilities/Utility.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R1.Automation.API.Core.Utilities
+{
+    public class Utility
+    {
+        
+
+        /// <summary>This is used for load appsettings.json file</summary>
+        public static IConfigurationRoot ConfigUrl
+        {
+            get
+            {
+                var config = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+                return config;
+            }
+        }
+
+        /// <summary>This method is used for Get Folder Path</summary>
+        /// <param name="appFolderName"></param>
+        /// <returns>This returns Path of a folder</returns>
+        public string GetFolderPath(string appFolderName)
+        {
+            var folderName = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            return Path.Combine(folderName.Substring(0, folderName.LastIndexOf("\\bin")), appFolderName + "\\");
+        }
+
+        /// <summary>This Method is used for read a json file</summary>
+        /// <param name="testDataPath"></param>
+        /// <returns>This returns string</returns>
+        public string LoadJson(string testDataPath)
+        {
+            using (StreamReader r = new StreamReader(testDataPath))
+            {
+                var json = r.ReadToEnd();
+                return JObject.Parse(json).ToString();
+
+            }
+        }
+
+        /// <summary>This method is used for get data from test data json file</summary>
+        /// <param name="key"></param>
+        /// <returns>This returns string value from test data json file</returns>
+        public string GetTestData(String key)
+        {
+            JObject obs = JObject.Parse(LoadJson(GetFolderPath(ConfigUrl["AppSettings:TestDataFolderName"]) + ConfigUrl["AppSettings:TestDataFileName"]));
+            return obs[key].ToString();
+        }
+
+        /// <summary>This method is used for get data from test query json file</summary>
+        /// <param name="key"></param>
+        /// <returns>This returns string value from test query json file</returns>
+        public string GetQueryData(String key)
+        {
+            JObject obs = JObject.Parse(LoadJson(GetFolderPath(ConfigUrl["AppSettings:TestQueryFolderName"]) + ConfigUrl["AppSettings:TestQueryFileName"]));
+            return obs[key].ToString();
+        }
+
+        /// <summary>This method is used for read a json file and replace some words</summary>
+        /// <param name="testDataPath"></param>
+        /// <param name="dict"></param>
+        /// <returns>This returns a string</returns>
+        public string LoadJsonReplace(string testDataPath, Dictionary<string, string> dict)
+        {
+            string jsonString = null;
+            using (StreamReader r = new StreamReader(testDataPath))
+            {
+                var json = r.ReadToEnd();
+                jsonString= JObject.Parse(json).ToString();
+
+            }
+
+            foreach (KeyValuePair<string, string> ele in dict)
+            {
+                jsonString = jsonString.Replace(ele.Key, ele.Value);
+            }
+
+            return jsonString;
+        }
+    }
+}
